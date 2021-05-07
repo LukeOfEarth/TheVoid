4c26d1868661c2048b45a5724bdfad2c13d60e1d
Luke van Aardt <vanaardtluke@gmail.com>
2021-05-07 18:38:06 +0200

    init commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+    console.log('New connection...');
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
